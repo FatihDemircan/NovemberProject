80eac1ebf8085f2d265c51218554e15b04aec9c2
FatihDemircan <fdfatihdemircan@gmail.com>
2020-12-16 12:32:26 -0500

    Batman has bat motorcycle

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -2,6 +2,7 @@ public class Batman {
     public static void main(String[] args) {
         System.out.println("I will catch you Joker!");
         // Yes he will catch him
-        // Batman has a cool Batmobile
+        // Batman has a cool Bat mobile
+        // Batman has a cool Bat motorcycle
     }
 }
